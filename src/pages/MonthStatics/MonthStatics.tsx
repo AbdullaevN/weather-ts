4dcaaec73287fcc1d64e9aa850e924d96f7ae0fc
AbdullaevN <nurlanabdullaev9820@gmail.com>
2022-12-03 01:43:32 +0600

    design

---
+++ src/pages/MonthStatics/MonthStatics.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from './MonthStatics.module.scss'
+
+interface  Props {
+
+}
+
+export const MonthStatics  = (props: Props) => {
+    return (
+        <div className={s.home}>
+            MonthStatics 
+        </div>
+    )
+}
